34c8b56a63687e277b32832bdbc83545733a3f85
Kevin <kevhines@gmail.com>
2021-02-22 13:28:52 -0500

    commit tree 10313ebcf3aa311281121de4323baa8895677721

--- app.rb
+++ app.rb
@@ -3,4 +3,49 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    reversed = params[:name].reverse
+    "The name reversed is: #{reversed}"
+  end
+
+  get '/square/:number' do
+    num = params[:number].to_i
+    "Squared: #{num * num}"
+   end
+
+
+  get '/say/:number/:phrase' do
+    num = params[:number].to_i
+    phrase = params[:phrase]
+    output = ""
+    num.times do
+      output << "#{phrase}"
+    end
+    output
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    result = ""
+    case operation
+    when "add"
+      result = "#{num1} + #{num2} = #{(num1 + num2)}"
+    when "subtract"
+      result = "#{num1} - #{num2} = #{(num1 - num2)}"
+    when "multiply"
+      result = "#{num1} * #{num2} = #{(num1 * num2)}"
+    when "divide"
+      result = "#{num1} / #{num2} = #{(num1 / num2)}"
+    end
+    result
+
+  end
+
 end
